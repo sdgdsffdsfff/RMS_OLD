e1a430840daaaa31ab258d2dc6179aa1dc021a10
bern <bern@22ed7b37-85c0-c74d-bab2-c74a3d316cc0>
2014-10-06 12:05:08 +0000

    修改录入动态字段类信息之前处理加载的Action，测试通过！
    		--》前台页面未跟换
    		--》hibernate数据过滤未添加

--- src/com/cqupt/mis/rms/action/teacher/InputDynamicDataRecordAction.java
+++ src/com/cqupt/mis/rms/action/teacher/InputDynamicDataRecordAction.java
@@ -8,29 +8,22 @@ import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
 /**
-*<p>Title:处理教师用户输入学生获奖信息之前的Action</p>
+*<p>Title:处理教师用户录入动态字段类信息之前的Action</p>
 *<p>Description:接收输入请求，取出动态的数据库字段，返回后展示在输入页面</p>
 *@author Bern
 *@version 2.0
 **/
-public class InputStudentAwardsAction extends ActionSupport {
+public class InputDynamicDataRecordAction extends ActionSupport {
 	//注入服务层接口
-	private DynamicDataFieldService dynamicDataFieldService;
 	private SearchDao searchDao;
 	
-	public String execute() {
+	/**
+	 * 录入学生获奖信息前的加载 
+	 */
+	public String inputStudentAwardsRecord() {
 		List<Object> fields = searchDao.SearchObjectsByFactor("com.cqupt.mis.rms.model.StudentAwardsField", "isDelete", 0);		
 		ActionContext.getContext().put("allFields", fields);
 		return SUCCESS;
-	}
-
-	public DynamicDataFieldService getDynamicDataFieldService() {
-		return dynamicDataFieldService;
-	}
-
-	public void setDynamicDataFieldService(
-			DynamicDataFieldService dynamicDataFieldService) {
-		this.dynamicDataFieldService = dynamicDataFieldService;
 	}
 
 	public SearchDao getSearchDao() {
