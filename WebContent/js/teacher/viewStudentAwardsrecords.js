a38a3fd0fd9efc351086e0ce623b430b9a79d53f
wangjingjing <wangjingjing@22ed7b37-85c0-c74d-bab2-c74a3d316cc0>
2014-11-02 02:59:52 +0000

    前台，动态生成表格

--- WebContent/js/teacher/viewStudentAwardsrecords.js
+++ WebContent/js/teacher/viewStudentAwardsrecords.js
@@ -7,20 +7,36 @@ var manager;
         {
             $.ligerDialog.alert(content);
         };
-        
         var gridManager = null;
         $(function ()
         {
-
-            //工具条
+        	//工具条
             $("#toptoolbar").ligerToolBar({ items: [
-                { text: '删除', id:'delete', click: itemclick, icon: 'delete' },{ line:true },
                 { text: '导出Excel', id:'Excel', click: itemclick, icon: 'save' },{ line:true },
+                { text: '修改', id:'modify', click: itemclick, icon: 'modify' },{ line:true },
+                { text: '查看详情', id:'detail', click: itemclick, icon: 'home' },{ line:true },
                 { text: '刷新', id:'refresh', click: itemclick, icon: 'refresh' }
             ]
             });
+            //列标题
+        	var json = $("#data").val();
+        	var objJson = JSON.parse(json);
+            var s1 = "{ \"display\": \"状态\", \"name\": \"Status\", \"align\": \"left\", \"width\": 100, \"minWidth\": 60 },{ \"display\": \"拒绝原因\", \"name\": \"refuse\", \"align\": \"left\", \"minWidth\": 140 },{ \"display\": \"信息名字\", \"name\": \"name\", \"width\": 120 }";
+            var colnames="";
+            var s3 ="\"dataAction\": \"server\", \"data\": \"rows\", \"sortName\": \"id\",\" width\": \"100%\", \"height\": \"100%\", \"pageSize\": 30,\"rownumbers\":true,\"checkbox\" : false,\"colDraggable\" : true,\"rowDraggable\" : true,\"cssClass\" : \"l-grid-gray\", \"heightDiff\": 0";
+            for(var i=1;i<=objJson.field.length;i++) //在这里读json的列名，当作表格的列名
+              {
+                  colnames+="{\"name\":\"value"+i+"\",\"minWidth\": 60, \"display\":\""+objJson.field[i-1].des+"\"},";
+              }
+              colnames=colnames+s1;
+              var col="{"+"\"columns\":["+colnames+"],"+s3+"}";
+              var colObj = JSON.parse(col);
+              g = manager = $grid = $("#maingrid").ligerGrid(colObj);
+	          show();
+	          gridManager = $("#maingrid").ligerGetGridManager();
+	          $("#pageloading").hide();	
         });
-
+      
         function showDetail(id)
         {
         	alert("您选中的ID是" + id);
@@ -28,14 +44,42 @@ var manager;
         
         function itemclick(item)
         { 
-            if(item.id)
+            if(item)
             {
                 switch (item.id)
                 {
                 	case "refresh":
                 	window.location.reload();
                     return;
-                    case "delete":
+                    case "modify":
+                    	 var data = gridManager.getCheckedRows();
+                         if (data.length == 0)
+                             alert('请选择行!');
+                         else
+                         {
+                             var checkedIds = [];
+                             var checkedNames = [];
+                             $(data).each(function ()
+                                     {
+                                     
+                                         	checkedIds.push(this.id);
+                                         	checkedNames.push(this.name);
+                                     });
+                            
+                            if(checkedIds.length == data.length)
+                            {
+                            	$.ligerDialog.confirm('确定修改:' + checkedNames.join(' ; ') + '?', function (result)
+                                {
+                                    if(result)
+                                    	{
+                                    		url = 'viewStudentAwardsRecordDetail.action?flag=modify'+'&recordId='+checkedIds;
+                                    		modifyInfo(url);
+                                        }
+                                });
+                             }
+                          }
+                        return;
+                    case "detail":
                         var data = gridManager.getCheckedRows();
                         if (data.length == 0)
                             alert('请选择行!');
@@ -44,30 +88,24 @@ var manager;
                             var checkedIds = [];
                             var checkedNames = [];
                             $(data).each(function ()
-                            {
-                            	if(parseInt(this.Status) != 0)
-                                {
-                                	alert('不能删除:'+this.rewardName);
-                                }else
-                                {
-                                	checkedIds.push(this.awardsId);
-                                	checkedNames.push(this.rewardName);
-                                }
-                            });
+                                    {
+                                    
+                                        	checkedIds.push(this.id);
+                                        	checkedNames.push(this.name);
+                                    });
                             
                             if(checkedIds.length == data.length)
                             {
-                            	$.ligerDialog.confirm('确定删除:' + checkedNames.join(' ; ') + '?', function (result)
+                            	$.ligerDialog.confirm('确定查看:' + checkedNames.join(' ; ') + '?', function (result)
                                 {
                                     if(result)
                                     	{
-                                    		url = 'deleteStudentAwards.action?infoIds='+checkedIds;
-                                    		deleteInfo(url);
+                                    		url = 'viewStudentAwardsRecordDetail.action?recordId='+checkedIds;
+                                    		detailInfo(url);
                                         }
                                 });
-                            }
-                           
-                        }
+                             }
+                          }
                         return;
                     case "Excel":
                     	var data = gridManager.getCheckedRows();
@@ -78,13 +116,13 @@ var manager;
                         	var checkedIds = [];
                         	$(data).each(function ()
                             {
-                        		checkedIds.push(this.awardsId);
+                        		checkedIds.push(this.id);
                             });
                         	$.ligerDialog.confirm('确定导出已选择的数据?', function (result)
                         	{
                         		if(result)
                         		{
-                        			link = 'downLoadExcelbyFactor/downloadStudentAwardsExcel.action?factorName=awardsId&factorValue='+checkedIds;
+                        			link = 'downLoadExcelbyFactor/downloadStudentAwardsExcel.action?factorName=id&factorValue='+checkedIds;
                         			downloadExcel(link);
                         		}
                         	});
@@ -126,30 +164,17 @@ var manager;
 		   }
     	   return xmlhttp;
     	}
-        
-        function deleteInfo(url)
+        function modifyInfo(url)
         {
-        	xmlhttp = GetxmlhttpObject();
-            xmlhttp.open("POST",url,true);
-            xmlhttp.onreadystatechange = showMessage;
-            xmlhttp.send(null);
+        	window.location.href=url;
         }
+        function detailInfo(url)
+        {
+        	window.location.href=url;
+        }
+      
         
-        function showMessage(){
-        	if(xmlhttp.readyState == 4){
-        		$.ligerDialog.waitting('删除中，请稍候...');
-        		setTimeout(function ()
-                {
-        			$.ligerDialog.closeWaitting();
-        			if(xmlhttp.responseText == "true"){
-        				gridManager.deleteSelectedRow();
-        				$.ligerDialog.success('删除成功！');
-        			}else{
-        				$.ligerDialog.error('删除失败！');
-        			}
-                },1500);
-        	}
-        }
+  
         
         function downloadExcel(link)
         {
@@ -157,14 +182,10 @@ var manager;
         }
         
         function getSelected()
-
         { 
 
             var row = manager.getSelectedRow();
-
             if (!row) { alert('请选择行'); return; }
-
-
         }
        
 	 function show()
